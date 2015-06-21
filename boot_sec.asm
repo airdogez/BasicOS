be7919f8a8de0fe2a85051f655fa84945ef43b80
Andres R <aqwssama@gmail.com>
2015-06-21 04:48:44 -0500

    Print string function

--- boot_sec.asm
+++ boot_sec.asm
@@ -1,24 +1,34 @@
-mov ah, 0x0e
+[org 0x7c00]
 
-mov bp, 0x8000
-mov sp, bp
+mov bx, HELLO_MSG
+call print_string
 
-push 'A'
-push 'B'
-push 'C'
-
-pop bx
-mov al, bl
-int 0x10
-
-pop bx
-mov al, bl
-int 0x10
-
-mov al, [0x7ffe]
-int 0x10
+mov bx, GOODBYE_MSG
+call print_string
 
 jmp $
+
+print_string:
+  pusha
+  ;Prints a string on screen
+  mov ah, 0x0e
+continue:
+  mov al, [bx]
+  cmp al, 0x00
+  je end_fun
+  int 0x10
+  add bx, 1
+  jmp continue
+end_fun:
+  popa
+  ret
+
+;DATA
+HELLO_MSG:
+  db 'Hello, World!', 0
+
+GOODBYE_MSG:
+  db 'Goodbye!', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
