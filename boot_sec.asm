63010570ec49afe0689e239fcbf627786ef5622a
Andres R <aqwssama@gmail.com>
2015-06-21 15:41:55 -0500

    Disk sector loader

--- boot_sec.asm
+++ boot_sec.asm
@@ -1,23 +1,30 @@
 [org 0x7c00]
 
-mov bx, HELLO_MSG
-call print_string
+mov [BOOT_DRIVE], dl
 
-mov bx, GOODBYE_MSG
-call print_string
+mov bp, 0x8000
+mov sp, bp
 
-mov dx, 0x101d
+mov bx, 0x9000
+mov dh, 5
+mov dl, [BOOT_DRIVE]
+call disk_load
+
+mov dx, [0x9000]
 call print_hex
 
-mov dx, 0x35fe
+mov dx, [0x9000 + 512]
 call print_hex
 
 jmp $
 
 %include "print_string.asm"
 %include "print_hex.asm"
+%include "disk_load.asm"
 
 ;DATA
+BOOT_DRIVE: db 0
+
 HELLO_MSG:
   db 'Hello, World!', 0
 
@@ -26,3 +33,6 @@ GOODBYE_MSG:
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
