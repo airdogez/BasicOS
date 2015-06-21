d0a5520af2ec3ca8a7a0101263c2e92d19e478e0
Andres R <aqwssama@gmail.com>
2015-06-21 04:26:31 -0500

    X

--- boot_sec.asm
+++ boot_sec.asm
@@ -1,17 +1,23 @@
 mov ah, 0x0e
 
-mov al, 'H'
+mov al, the_secret
 int 0x10
-mov al, 'e'
+
+mov al, [the_secret]
 int 0x10
-mov al, 'l'
+
+mov bx, the_secret
+add bx, 0x7c00
+mov al, [bx]
 int 0x10
-mov al, 'l'
-int 0x10
-mov al, 'o'
+
+mov al, [0x7c1e]
 int 0x10
 
 jmp $
+
+the_secret:
+  db "X"
 
 times 510-($-$$) db 0
 dw 0xaa55
