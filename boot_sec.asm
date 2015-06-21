03ec8a1ca78f239f173888f30f8bc9bb7c36dbec
Andres R <aqwssama@gmail.com>
2015-06-21 04:29:34 -0500

    Stack test

--- boot_sec.asm
+++ boot_sec.asm
@@ -1,23 +1,24 @@
 mov ah, 0x0e
 
-mov al, the_secret
+mov bp, 0x8000
+mov sp, bp
+
+push 'A'
+push 'B'
+push 'C'
+
+pop bx
+mov al, bl
 int 0x10
 
-mov al, [the_secret]
+pop bx
+mov al, bl
 int 0x10
 
-mov bx, the_secret
-add bx, 0x7c00
-mov al, [bx]
-int 0x10
-
-mov al, [0x7c1e]
+mov al, [0x7ffe]
 int 0x10
 
 jmp $
-
-the_secret:
-  db "X"
 
 times 510-($-$$) db 0
 dw 0xaa55
