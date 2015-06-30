47524dc8a746462ffee2b4b2a06deffcedc99cd1
Andres Revolledo Galvez <aqwssama@gmail.com>
2015-06-29 22:31:01 -0500

    Comments

--- boot/disk_load.asm
+++ boot/disk_load.asm
@@ -1,18 +1,18 @@
 ;Load DH sectors to ES:BX from drive DL
 disk_load:
-  push dx
-  mov ah, 0x02
-  mov al, dh
-  mov ch, 0x00
-  mov dh, 0x00
-  mov cl, 0x02
+  push dx ;Guarda DX
+  mov ah, 0x02 ;indica leer de un dispositivo
+  mov al, dh ;La cantidad de sectores
+  mov ch, 0x00 ;El cilindro
+  mov dh, 0x00 ;La cabeza
+  mov cl, 0x02 ;El sector
 
-  int 0x13
+  int 0x13 ;LLama a la interrupcion
   
-  jc disk_error
+  jc disk_error ;Si CF = 1 salta al sector de error
 
-  pop dx
-  cmp dh, al
+  pop dx ;Recupera DX
+  cmp dh, al ;Verifica que se haya leido todo
   jne disk_error
   ret
 
--- boot/print_string.asm
+++ boot/print_string.asm
@@ -1,14 +1,14 @@
 ;Prints a string on screen
 print_string:
-  pusha
-  mov ah, 0x0e
+  pusha ;Guardamos los registros
+  mov ah, 0x0e ;Indica a la interrupcion que funcion utilizar: Imprimir Caracter
 continue:
-  mov al, [bx]
-  int 0x10
-  cmp al, 0x00
-  je end_fun
-  add bx, 1
-  jmp continue
+  mov al, [bx] ;Mueve a AL la direccion donde se encuentra el caracter a imprimir
+  int 0x10 ;Llama a la interrupcion
+  cmp al, 0x00 ;Verifica que no este en el ultimo caracter de la cadena
+  je end_fun ;Si es asi termina la funcion
+  add bx, 1 ;Incrementa la posicion de memoria
+  jmp continue ;Vuelve a realizar el proceso de impresion de caracter
 end_fun:
-  popa
+  popa ;Recuperamos los registros
   ret
